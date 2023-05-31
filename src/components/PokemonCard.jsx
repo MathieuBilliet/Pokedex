53cde31d6d10894ef8f49ecab4ef65f6ad0a5f1f
Mathieu-B <Mathieubilliet@gmail.com>
2023-05-31 12:14:39 +0200

    Quetes 3 react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,30 @@
+const pokemonList = [
+    {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+    name: "mew",
+    },
+    ];
+
 function PokemonCard() {
-return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"></img>
-    <figcaption>"bulbasaur"</figcaption>
-</figure>
+
+const pokemon = pokemonList[0];
+
+return (<>{pokemon.imgSrc != undefined ? (
+<img
+src={pokemon.imgSrc} 
+alt={pokemon.name} />
+) : (
+<p>"???"</p> )}
+
+<figcaption>{pokemon.name}</figcaption>
+</>
+);
 }
+
+
+
+
 export default PokemonCard;
