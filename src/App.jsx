d5b8937aa4ff0c4c6e3dcf171f251394cfdb4535
Mathieu-B <Mathieubilliet@gmail.com>
2023-06-01 17:34:04 +0200

    quetes pokemonIndex

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,58 @@
-import './App.css'
-import MyTitle from './components/Mytitle'
-import PokemonCard from './components/PokemonCard'
+import "./App.css";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
-function App() {
-
-  const pokemonList = [
+const pokemonList = [
   {
-  name: "bulbasaur",
-  imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-  name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
-  ];
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+const App = () => {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
 
-  const selectedPokemon = pokemonList[0];
+  const handleClickSui = () =>
+    setPokemonIndex(
+      pokemonIndex < pokemonList.length - 1 ? pokemonIndex + 1 : pokemonIndex
+    );
+
+  const handleClickPre = () =>
+    setPokemonIndex(pokemonIndex > 0 ? pokemonIndex - 1 : pokemonIndex);
+
+  const Pokemon = pokemonList[pokemonIndex];
 
   return (
-    <>
-      <PokemonCard pokemon={selectedPokemon} />
-    </>
+    <div>
+      <img src={Pokemon.imgSrc} alt={Pokemon.name} />
+      <div>
+        {pokemonIndex > 0 ? (
+          <button onClick={handleClickPre}>Précédent</button>
+        ) : undefined}
+        {pokemonIndex < pokemonList.length - 1 ? (
+          <button onClick={handleClickSui}>Suivant</button>
+        ) : undefined}
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
