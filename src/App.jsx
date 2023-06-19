b18e772e4826affccbe5ccf7f5bee63dcbc6d3a5
Mathieu-B <Mathieubilliet@gmail.com>
2023-06-19 13:55:27 +0200

    Finish quetes pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,31 @@ import MyTitle from "./components/MyTitle/MyTitle";
 
 const pokemonList = [
   {
+    id: 0,
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    id: 1,
     name: "charmander",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
   {
+    id: 2,
     name: "squirtle",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
   },
   {
+    id: 3,
     name: "pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
+    id: 4,
     name: "mew",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
@@ -43,15 +48,18 @@ const App = () => {
       /* pokemonList est passé en props pour vérifier la taille du tableau pour afficher au nonn le boutton suivant */
       /* pokemonIndex est passé en props pour vérifier si on est au début ou à la fin du tableau pour afficher au nonn le boutton suivant */
       /* setPokemonIndex est passé en props pour modifier l'index du pokemon à afficher en fonction du boutton cliqué */}
-      <NavBar
-        pokemonList={pokemonList}
-        pokemonIndex={pokemonIndex}
-        setPokemonIndex={setPokemonIndex}
-      />
-      <MyTitle />
       <div className="CardContainer">
+        <MyTitle />
         <img src={Pokemon.imgSrc} alt={Pokemon.name} />
         <h1>{Pokemon.name} </h1>
+      </div>
+
+      <div className="navBar">
+        <NavBar
+          pokemonList={pokemonList}
+          pokemonIndex={pokemonIndex}
+          setPokemonIndex={setPokemonIndex}
+        />
       </div>
     </>
   );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,39 +6,25 @@ import "./NavBar.css";
 // pokemonIndex est passé en props pour vérifier si on est au début ou à la fin du tableau pour afficher au nonn le boutton suivant
 // setPokemonIndex est passé en props pour modifier l'index du pokemon à afficher en fonction du boutton cliqué
 
-const NavBar = ({ pokemonList, pokemonIndex, setPokemonIndex }) => {
-  // Déclaration de deux fonctions handleClickSui et handleClickPre
-
-  // handleClickSui permet d'incrémenter l'index du pokemon à afficher de 1 si l'index est inférieur à la taille du tableau - 1
-  // sinon l'index reste le même
-
-  const handleClickSui = () =>
-    setPokemonIndex(
-      pokemonIndex < pokemonList.length - 1 ? pokemonIndex + 1 : pokemonIndex
-    );
-
-  // handleClickPre permet de décrémenter l'index du pokemon à afficher de 1 si l'index est supérieur à 0
-  // sinon l'index reste le même
-  const handleClickPre = () =>
-    setPokemonIndex(pokemonIndex > 0 ? pokemonIndex - 1 : pokemonIndex);
+const NavBar = ({ pokemonList, setPokemonIndex }) => {
+  const handleClick = (index) => {
+    setPokemonIndex(index);
+  };
 
   return (
-    <nav>
-      <div className="buttonContainer">
-        {pokemonIndex > 0 ? (
-          // Si pokemonIndex est supérieur à 0, on affiche le boutton précédent
-          // Ce boutton déclenche la fonction handleClickPre au clic permet de décrémenter l'index du pokemon à afficher de 1 si l'index est supérieur à 0
-          <button onClick={handleClickPre}>Précédent</button>
-        ) : // Sinon on n'affiche rien
-        undefined}
-
-        {pokemonIndex < pokemonList.length - 1 ? (
-          // Si pokemonIndex est inférieur à la taille du tableau - 1, on affiche le boutton suivant */
-          // Ce boutton déclenche la fonction handleClickSui au clic permet d'incrémenter l'index du pokemon à afficher de 1 si l'index est inférieur à la taille du tableau - 1
-          <button onClick={handleClickSui}>Suivant</button>
-        ) : // Sinon on n'affiche rien
-        undefined}
-      </div>
+    <nav className="MenuNav">
+      <ul>
+        {pokemonList.map((pokemon) => (
+          <button className="InputFive" key={pokemon.id}>
+            <input
+              className="inputpok"
+              type="button"
+              value={pokemon.name}
+              onClick={() => handleClick(pokemon.id)}
+            />
+          </button>
+        ))}
+      </ul>
     </nav>
   );
 };
